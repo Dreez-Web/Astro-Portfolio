7f144a9d99a3dc02d378cd12cf9c67972b411d5f
Dreez-web <alpuis.andres@gmail.com>
2025-01-17 17:11:16 -0300

    Seccion contacto + formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Dreez-Web.github.io",
   adapter: vercel({ imageService: true }),
+  integrations: [tailwind()],
 });
